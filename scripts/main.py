f5fd83d7126911647eb5670a0fe73e77175f9451
Liam Maarhuis <liammaarhuis@gmail.com>
2026-04-07 13:37:14 -0700

    final update with working code added multiple checks for validation added retrying collection if input is already inside program

--- scripts/main.py
+++ scripts/main.py
@@ -2,32 +2,11 @@ import yfinance
 import datetime
 import time
 import oracledb
-
-def example1():
-    ticker_String = "AAPL"
-    ticker = yfinance.Ticker(ticker_String)
-
-    historical_data = ticker.history(period="1mo")  # data for the last month
-
-    #Display a summary of the fetched data
-    print(f"Summary of Historical Data for {ticker_String}:")
-    print(historical_data[['Open', 'High', 'Low', 'Close', 'Volume']])
-    print(historical_data['Low'])
-    print(historical_data.tail(1))
-
-
-def message_handler(message):
-    print("Received message:", message)
-
-
-def example2():
-    with yfinance.WebSocket() as ws:
-        ws.subscribe(["AAPL", "BTC-USD"])
-        ws.listen(message_handler)
+import pandas
 
 
 def connecttoDB():
-    print("Connecting to Oracledb")
+    print("-- Connecting to Oracledb")
     db = oracledb.connect(
         user="DEV4",
         password="Comp430proj!",
@@ -35,16 +14,12 @@ def connecttoDB():
         wallet_location="C:/Database_Utils/Wallet_stockAlgorithimicTrading",
         wallet_password="Comp430proj!"
     )
-    print("Connected to Oracledb user "+ db.username)
+    print("-- Connected to Oracledb user "+ db.username)
     return db
-
-# only one 'DATEKEY' 'STOCKINFOKEY' 'SOURCEKEY' when checking if input is valid
-def convertdatetokeys(date):
-    #converts datetime to key's found in db
-    pass
 
 
 def getData(toget,db):
+    print("Getting " +toget+ " from db")
     sql = '''SELECT * FROM '''+toget
     cursor = db.cursor()
 
@@ -56,8 +31,8 @@ def getData(toget,db):
 
 
 def insertData(params,db):
-    print("--------------- inside insert ---------------")
-    sql = '''INSERT INTO DEV4.TEST_STOCK_FACT(STOCKFACTKEY, DATEKEY, STOCKINFOKEY, SOURCEKEY, OPEN_PRICE, VOLUME, TIME_KEY, HIGH_PRICE ,LOW_PRICE) VALUES(:STOCKFACTKEY, :DATEKEY, :STOCKINFOKEY, :SOURCEKEY, :OPEN_PRICE, :VOLUME, :TIME_KEY, :HIGH_PRICE , :LOW_PRICE)'''
+    #print("--------------- inside insert ---------------")
+    sql = '''INSERT INTO DEV.MARKET_FACT(DATEKEY, ASSETKEY, SOURCEKEY, OPEN_PRICE, VOLUME, TIME_KEY, HIGH_PRICE ,LOW_PRICE) VALUES(:DATEKEY, :ASSETKEY, :SOURCEKEY, :OPEN_PRICE, :VOLUME, :TIME_KEY, :HIGH_PRICE , :LOW_PRICE)'''
 
     cursor = db.cursor()
     try:
@@ -67,50 +42,165 @@ def insertData(params,db):
         print("Error:", e)
 
 
-def main():
-    loopcounter = 0
-    ticker = yfinance.Ticker("AAPL")
-    db = connecttoDB()
-    factdata = getData('''DEV4.TEST_STOCK_FACT''',db)
-    print(factdata[-1][0])
+def getDateKey(date_dim,time_dim,stockdatetime):
+    currentdatetime = stockdatetime.to_pydatetime()
+    datekey = -1
+    timekey = -1
+
+    for i in date_dim:
+        if i[0] == currentdatetime.day and i[1] == currentdatetime.month and i[3] == currentdatetime.year:
+            datekey = i[5]
+            break
+
+    for i in time_dim:
+        if i[1] == currentdatetime.hour and i[2] == roundTime(currentdatetime.minute):
+            timekey = i[0]
+            break
+
+    return datekey, timekey
 
 
-    #Main loop for grabbing data from yfinance and uploading it to oracledb
-    while True:
-        try:
-            data = ticker.history(period="1d", interval="1m")
-            latest = data.iloc[-2]
+def roundTime(mins):
+    if mins >= 0 and mins < 15:
+        return 0
+    elif mins >= 15 and mins < 30:
+        return 15
+    elif mins >= 30 and mins < 45:
+        return 30
+    elif mins >= 45 and mins <= 60:
+        return 45
+    else:
+        return -1
 
-            print(latest)
-            print("Datetime: "+ str(latest.name))
-            print("Open: "+str(latest.Open))
-            print("High: "+str(latest.High))
-            print("Low: "+str(latest.Low))
-            print("Close: "+str(latest.Close))
-            print("Volume: "+str(latest.Volume))
+# Grabs given assetname's asset key and asset type from asset_dim
+def getAssetInfo(asset_dim,assetname):
+    assetkey = -1
+    assettype = -1
+    for i in asset_dim:
 
-            params = {
-                'STOCKFACTKEY': factdata[-1][0] + 1 + loopcounter, #<-- grabs the last entery in fact table and sets the fact key to 1 + it's fact key value + loopcounter
-                'DATEKEY': 1,
-                'STOCKINFOKEY': 1,
-                'SOURCEKEY': 1,
-                'OPEN_PRICE': latest.Open,
-                'VOLUME': latest.Volume,
-                'TIME_KEY': 1,
-                'HIGH_PRICE': latest.High,
-                'LOW_PRICE': latest.Low
-            }
-            print(params)
-            #insertData(params,db)
+        if assetname == i[1]:
+            assetkey = i[0]
+            assettype = i[5]
+            return assetkey, assettype
+    return assetkey, assettype
 
-            #print(datetime.datetime.now(), latest)
+def checkDuplicate(market_dim,datekey,timekey,assetkey):
+    sourcekey = 1
+    for i in market_dim:
+        #print("datekey: "+str(datekey)+" . "+str(i[1])+" | timekey: "+str(timekey)+" . "+str(i[6])+" | assetkey: "+str(assetkey)+" . "+str(i[2]))
+        if datekey == i[1] and timekey == i[6] and assetkey == i[2] and sourcekey == i[3]:
+            return True
 
-        except Exception as e:
-            print("Error:", e)
-        loopcounter += 1
+    return False
+
+
+def checkValidInt(tocheck):
+    if tocheck <= 0:
+        return None
+    return tocheck
+
+
+def checkValidKeys(key):
+    if key == -1:
+        return False
+    return True
+
+
+def checkVolume(latest):
+    if latest.Volume < 0:
+        return -1
+    if latest.Volume == 0 and latest.Open != latest.Close:
+        return -1
+    return latest.Volume
+
+
+def singleStock(db, assetname, date_dim, time_dim, asset_dim):
+
+    # collect assetkey
+    assetkey, assettype = getAssetInfo(asset_dim,assetname)
+    # gather yfinance data if stock exsists in asset_dim
+    if assetkey == -1:
+        print("------- "+assetname+" does not exsist inside of database, please check assetnamelist and correct to valid input")
+        return
+    else:
+        ticker = yfinance.Ticker(assetname)
+        data = ticker.history(period="1d", interval="15m")
+        latest = data.iloc[-2]
+
+    #gets the market fact table that is isolated to the stock or bond in question
+    market_fact = getData(f'''DEV.MARKET_FACT WHERE ASSETKEY = {assetkey}''',db)
+
+    #collect date and time keys
+    datekey, timekey = getDateKey(date_dim,time_dim,latest.name)
+
+
+# checks to see if values are non-zero and non-negative, if so set to None
+    latest.Open = checkValidInt(latest.Open)
+    latest.High = checkValidInt(latest.High)
+    latest.Low = checkValidInt(latest.Low)
+    latest.Close = checkValidInt(latest.Close)
+    #checks that volume is non zero, as well comfirms that if it is zero that the open and close prices are the same
+    if assettype != "Bond":
+        latest.Volume = checkVolume(latest)
+
+    params = {
+        'DATEKEY': datekey,
+        'ASSETKEY': assetkey,
+        'SOURCEKEY': 1, #always 1 since this is the refferance to the yfinance sourcekey inside the source_dim
+        'OPEN_PRICE': latest.Open,
+        'VOLUME': latest.Volume,
+        'TIME_KEY': timekey,
+        'HIGH_PRICE': latest.High,
+        'LOW_PRICE': latest.Low
+    }
+
+    complete = False
+    if not checkDuplicate(market_fact,datekey,timekey,assetkey):
+        if checkValidKeys(latest.Volume):
+            # checks both datekey and timekey to assure that they are not == -1, as if they are then they are not valid
+            if checkValidKeys(datekey) and checkValidKeys(timekey):
+                print("------- Inserting data for "+assetname+" with asset key "+str(assetkey)+" to db")
+                insertData(params,db)
+                complete = True
+            else:
+                print("---- Refusing to input due to invalid date time keys")
+                complete = True
+        else:
+            print("---- Refusing to input due to invalid Volume")
+            complete = True
+    else:
+        print("---- Refusing to input due to params already being inside fact table")
+    if complete == False:
+        print("Failed to collect. Waiting 1 min then retrying")
         time.sleep(60)
+        singleStock(db, assetname, date_dim, time_dim, asset_dim)
+        print("Retrying collection")
+
+def main():
+
+    running = True
+    assetnamelist = ["GOOGL","^FVX","GC=F","^DJI"]
+    db = connecttoDB()
 
 
-#example1()
-#example2()
+    ##Gets all the dims for compliance checks
+    date_dim = getData("DEV.DATE_DIM",db) # day = 0 month = 1 year = 3 datekey = 5
+    time_dim = getData("DEV.TIME_DIM",db)
+    asset_dim = getData("DEV.ASSET_DIM",db)
+    while running:
+        starttime = time.time()
+        for i in assetnamelist:
+
+            try:
+                singleStock(db, i, date_dim, time_dim, asset_dim)
+            except Exception as e:
+                print("Error:", e)
+        print("---------------- completed loop ----------------")
+        #used to consistantly run the program every 15 min. Done by subtracting the 15min (900s) by the runtime.
+        sleeptime = max(0,900 - (time.time() - starttime))
+        time.sleep(sleeptime)
+
+
+
+# run le program
 main()
